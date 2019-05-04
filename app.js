099de4da9d64867fa09dfe5be3ca893a9d152a90
coga <takahiro.koga@tns.co.jp>
2019-05-05 00:06:54 +0900

    add call weather api

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
+const request = require('request');
+
 var fs = require('fs'),
 props = require('props');
 var yamlData = fs.readFileSync('config/application-develop.yml','utf8');
 var config = props(yamlData);
-console.log( config.api );
+
+// call livedoor weather-api
+request.get({
+    uri: config.api.livedoor.base_url,
+    headers: {'Content-type': 'application/json'},
+    qs: {
+        output: "json",
+        city: config.api.livedoor.city
+    },
+    json: true
+}, function(err, req, data){
+    let result = {};
+    data.forecasts.forEach((obj) => {
+      console.log(obj.dateLabel);
+      console.log(obj.telop);
+    });
+});
