446ac9bd3e24f07f3d44261614527351b6e8d0a3
coga <takahiro.koga@tns.co.jp>
2019-05-05 21:45:58 +0900

    call chatapi

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const INDENTION = '\n';
 
 var fs = require('fs'),
 props = require('props');
@@ -7,17 +8,68 @@ var config = props(yamlData);
 
 // call livedoor weather-api
 request.get({
-    uri: config.api.livedoor.base_url,
-    headers: {'Content-type': 'application/json'},
-    qs: {
-        output: "json",
-        city: config.api.livedoor.city
-    },
-    json: true
+  uri: config.api.livedoor.base_url,
+  headers: {'Content-type': 'application/json'},
+  qs: {
+      output: "json",
+      city: config.api.livedoor.city
+  },
+  json: true
 }, function(err, req, data){
-    let result = {};
-    data.forecasts.forEach((obj) => {
-      console.log(obj.dateLabel);
-      console.log(obj.telop);
-    });
+  request.post({
+    uri: config.api.chatwork.base_url + '/rooms/' + config.api.chatwork.room_id + '/messages',
+    headers: {
+      'Content-type': 'application/json',
+      "X-ChatWorkToken": config.api.chatwork.api_key
+    },
+    form: {
+        'body' : makeSendMsg(data)
+    }
+}, (err, res, data) => {
+    console.log(data);
 });
+
+
+});
+
+// chatwork送付用のメッセージ文面作成関数
+function makeSendMsg(data) {
+  var weatherCmnt = ['[info][title]☀︎']
+  // title
+  weatherCmnt.push(data.title)
+  weatherCmnt.push('☀︎[/title]')
+  data.forecasts.forEach((obj, index) => {
+    if(index == 0){
+      // 今日の天気
+      weatherCmnt.push(obj.dateLabel);
+      weatherCmnt.push('の天気は');
+      weatherCmnt.push(obj.telop);
+      weatherCmnt.push('だよ');
+      var tempe = [INDENTION];
+      if(obj.temperature.min != null){
+        tempe.push('最低気温は');
+        tempe.push(obj.temperature.min.celsius);
+        tempe.push('℃ ');
+      }
+      if(obj.temperature.max != null){
+        tempe.push('最高気温は');
+        tempe.push(obj.temperature.max.celsius);
+        tempe.push('℃');
+      }
+      if(tempe.length > 0){
+        // 気温情報が取得できた場合
+        weatherCmnt = weatherCmnt.concat(tempe);
+      }
+      return;
+    }
+    // 明日以降の天気
+    weatherCmnt.push(INDENTION);
+    weatherCmnt.push('ちなみに');
+    weatherCmnt.push(obj.dateLabel);
+    weatherCmnt.push('の天気は');
+    weatherCmnt.push(obj.telop);
+    weatherCmnt.push('だよ');
+  });
+  weatherCmnt.push('[/info]');
+  return weatherCmnt.join('');
+}
